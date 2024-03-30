57794173494aa037a1569017f72d67b917040068
oba2311 <oba2311@gmail.com>
2024-03-30 18:31:16 +0300

    added GA tag

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"./*{html,js}",
+		// add other file types if necessary
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
