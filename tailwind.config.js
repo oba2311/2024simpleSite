ed88d5d455d2698a1c0c09cfb31e059644e7d2f4
oba2311 <oba2311@gmail.com>
2024-12-29 12:49:54 +0200

    changes a-al-Isenbergs style
    Enhance website structure and styling
    
    - Updated index.html with a new title and meta description for improved SEO.
    - Refactored CSS to utilize Tailwind CSS, including new utility classes and custom styles.
    - Added PostCSS configuration for Tailwind integration.
    - Updated package.json scripts for better development workflow.
    - Included new assets (images and fonts) for enhanced visual appeal.
    - Cleaned up .gitignore to exclude unnecessary files and directories.
    - Reduced package-lock.json size by removing unused dependencies.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{js,jsx,ts,tsx,html}", "./index.html"],
+	content: ["./*.{html,js}"],
 	theme: {
 		extend: {
 			fontFamily: {
-				"neue-montreal": ['"Neue Montreal"', "sans-serif"],
+				caveat: ["Caveat", "cursive"],
 			},
 		},
 	},
